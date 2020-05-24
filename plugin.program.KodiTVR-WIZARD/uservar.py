ba2bf19769cc12b93b7bd34e1190b8c25d31c594
KodiTVR-add-on <>
2020-05-24 17:47:37 +0100

    Update uservar.py

--- plugin.program.KodiTVR-WIZARD/uservar.py
+++ plugin.program.KodiTVR-WIZARD/uservar.py
@@ -1,32 +1,96 @@
 import os, xbmc, xbmcaddon
-
+import binascii
 #########################################################
 ### User Edit Variables #################################
 #########################################################
-ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
-ADDONTITLE     = 'KodiTVR-WIZARD'
-EXCLUDES       = [ADDON_ID]
-# Text File with build info in it.
-BUILDFILE      = 'https://pastebin.com/raw/wPzsuK9q'
-# How often you would list it to check for build updates in days
-# 0 being every startup of kodi
-UPDATECHECK    = 0
-# Text File with apk info in it.
-APKFILE        = 'https://'
-# Text File with Youtube Videos urls.  Leave as 'http://' to ignore
-YOUTUBETITLE   = ''
-YOUTUBEFILE    = 'https://'
-# Text File for addon installer.  Leave as 'http://' to ignore
-ADDONFILE      = 'https://'
-# Text File for advanced settings.  Leave as 'http://' to ignore
-ADVANCEDFILE   = 'http://'
+# Enable/Disable the text file caching with 'Yes' or 'No' and age being how often it rechecks in minutes
+CACHETEXT      = 'Yes'
+CACHEAGE       = 30
 
-# Dont need to edit just here for icons stored locally
+ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
+ADDONTITLE     = '[B][COLOR yellow]KodiTVR-WIZARD[/COLOR][/B]'
+BUILDERNAME    = 'WIZARD KodiTVR '
+#########################Make sure to change the repo to yours!!!!
+EXCLUDES       = [ADDON_ID, 'My_Builds', 'backupdir']
+BUILDFILE      = 'https://archive.org/download/KodiTVR-WIZARD/KodiTVR-WIZARD.zip'
+UPDATECHECK    = 0
+APKFILE        = 'https://'
+YOUTUBETITLE   = 'http://'
+YOUTUBEFILE    = 'https://'
+ADDONFILE      = 'https://cld.pt/dl/download/b7ed3c20-4710-4737-9ca9-0348f8a08939/addons.txt?download=true'
+ADVANCEDFILE   = 'https://cld.pt/dl/download/69d0206f-3b06-4c72-bcb3-943ff362da11/krypton%20advanced.txt?download=true'
+ROMPACK        = ''
+EMUAPKS        = ''
+ADDONPACK      = 'http://'
 PATH           = xbmcaddon.Addon().getAddonInfo('path')
 ART            = os.path.join(PATH, 'resources', 'art')
 
 #########################################################
 ### THEMING MENU ITEMS ##################################
+#########################################################
+
+##Alway test to see the color combo!!
+
+#### NEW GUI THEME ###################################
+# Choose from the following 
+# Only these colors avalable
+# white , blue , orange , yellow , red , purple , pink , lime , cyan, green
+#Button focus color
+FOCUS_BUTTON_COLOR = 'yellow'
+EXIT_BUTTON_COLOR = 'white'
+#Highlight outline for lists
+HIGHLIGHT_LIST = 'white'
+##No TXT file Banner
+NO_TXT_FILE = 'red'
+
+############################################
+############################################
+### The full list of colors for below can found @ https://forum.kodi.tv/showthread.php?tid=210837
+
+#Top Main buttons
+MAIN_BUTTONS_TEXT = 'white'
+#All other buttons
+OTHER_BUTTONS_TEXT = 'yellow'
+#all list text color
+##FYI any color placed in the txt file will overide this
+LIST_TEXT = 'yellow'
+
+
+#Description text title color
+DES_T_COLOR = 'yellow'
+#Description color
+DESCOLOR = 'white'
+
+#Wizard title name and verion color
+WIZTITLE = 'WIZARD KodiTVR'
+WIZTITLE_COLOR = 'yellow'
+VERTITLE_COLOR = 'white'
+VER_NUMBER_COLOR = 'yellow'
+############################################################
+
+## The colors and theme below is still used for the pop up dialogs
+##Alway test to see the color combo
+# You can edit these however you want, just make sure that you have a %s in each of the
+# THEME's so it grabs the text from the menu item
+COLOR1         = 'blue'
+COLOR2         = 'yellow'
+COLOR3         = 'red'
+COLOR4         = 'snow'
+COLOR5         = 'lime'
+# Primary menu items   / %s is the menu item and is required
+THEME1         = '[COLOR '+COLOR1+']%s[/COLOR]'
+# Build Names          / %s is the menu item and is required
+THEME2         = '[COLOR '+COLOR1+']%s[/COLOR]'
+# Alternate items      / %s is the menu item and is required
+THEME3         = '[COLOR '+COLOR2+']%s[/COLOR]'
+# Current Build Header / %s is the menu item and is required
+THEME4         = '[COLOR '+COLOR2+']Current Build:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+# Current Theme Header / %s is the menu item and is required
+THEME5         = '[COLOR '+COLOR2+']Current Theme:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+THEME6         = '[COLOR '+COLOR3+'][B]%s[/B][/COLOR]'
+
+
+
 #########################################################
 # If you want to use locally stored icons the place them in the Resources/Art/
 # folder of the wizard then use os.path.join(ART, 'imagename.png')
@@ -34,7 +98,7 @@ ART            = os.path.join(PATH, 'resources', 'art')
 # Example:  ICONMAINT     = os.path.join(ART, 'mainticon.png')
 #           ICONSETTINGS  = 'http://aftermathwizard.net/repo/wizard/settings.png'
 # Leave as http:// for default icon
-ICONBUILDS     = 'https://i.imgur.com/3spstUk.png'
+ICONBUILDS     = 'https://i.imgur.com/7CIT93Q.png'
 ICONMAINT      = 'https://i.imgur.com/YFw2062.png'
 ICONAPK        = 'https://i.imgur.com/cGG1z5S.png'
 ICONADDONS     = 'https://i.imgur.com/HlvkAYR.png'
@@ -48,31 +112,16 @@ ICONSETTINGS   = 'https://i.imgur.com/kOVekP3.png'
 # Hide the ====== seperators 'Yes' or 'No'
 HIDESPACERS    = 'No'
 # Character used in seperator
-SPACER         = '='
-
-# You can edit these however you want, just make sure that you have a %s in each of the
-# THEME's so it grabs the text from the menu item
-COLOR1         = 'lime'
-COLOR2         = 'gold'
-# Primary menu items   / %s is the menu item and is required
-THEME1         = '[COLOR '+COLOR1+'][B][I]([COLOR '+COLOR2+']KodiTVR-WIZARD[/COLOR])[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR][/I]'
-# Build Names          / %s is the menu item and is required
-THEME2         = '[COLOR '+COLOR2+']%s[/COLOR]'
-# Alternate items      / %s is the menu item and is required
-THEME3         = '[COLOR '+COLOR1+']%s[/COLOR]'
-# Current Build Header / %s is the menu item and is required
-THEME4         = '[COLOR '+COLOR1+']Version actual:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
-# Current Theme Header / %s is the menu item and is required
-THEME5         = '[COLOR '+COLOR1+']Tema Actual:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+SPACER         = '~'
 
 # Message for Contact Page
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
 HIDECONTACT    = 'No'
 # You can add \n to do line breaks
-CONTACT        = 'Gracias por elegir KodiTVR-WIZARD.'
+CONTACT        = 'Obrigado por usar o WIZARD KodiTVR.'
 #Images used for the contact window.  http:// for default icon and fanart
-CONTACTICON    = 'https://i.imgur.com/3spstUk.png'
-CONTACTFANART  = 'https://i.imgur.com/RuSSUhU.jpg'
+CONTACTICON    = os.path.join(ART, 'icon.png')
+CONTACTFANART  = 'https://cld.pt/dl/download/63930508-7559-47f9-a40e-ea4684e5cb21/fanart.jpg?download=true'
 #########################################################
 
 #########################################################
@@ -81,7 +130,7 @@ CONTACTFANART  = 'https://i.imgur.com/RuSSUhU.jpg'
 # Enable Auto Update 'Yes' or 'No'
 AUTOUPDATE     = 'Yes'
 # Url to wizard version
-WIZARDFILE     = 'https://pastebin.com/raw/wPzsuK9q'
+WIZARDFILE     = 'https://archive.org/download/KodiTVR-WIZARD/KodiTVR-WIZARD.zip'
 #########################################################
 
 #########################################################
@@ -92,9 +141,9 @@ AUTOINSTALL    = 'No'
 # Addon ID for the repository
 REPOID         = ''
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
-REPOADDONXML   = ''
+REPOADDONXML   = 'http://'
 # Url to folder zip is located in
-REPOZIPURL     = ''
+REPOZIPURL     =  'http://'
 #########################################################
 
 #########################################################
@@ -106,9 +155,13 @@ ENABLE         = 'Yes'
 NOTIFICATION   = 'https://pastebin.com/raw/q6XsXfx8'
 # Use either 'Text' or 'Image'
 HEADERTYPE     = 'Text'
-HEADERMESSAGE  = 'KodiTVR-WIZARD'
-# url to image if using Image 500x50(Width can vary but height of image needs to be 50px)
-HEADERIMAGE    = ''
+# Font size of header
+FONTHEADER     = 'Font25'
+HEADERMESSAGE  = 'WIZARD KodiTVR'
+# url to image if using Image 424x180
+HEADERIMAGE    = 'https://i.imgur.com/3spstUk.png'
+# Font for Notification Window
+FONTSETTINGS   = 'Font12'
 # Background for Notification Window
-BACKGROUND     = ''
-#########################################################
+BACKGROUND     = 'https://i.imgur.com/RuSSUhU.jpg'
+############################WIZARD KodiTVR#############################
